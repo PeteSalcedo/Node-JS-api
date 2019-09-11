4f3a5e934d06fadfbf29c77006c6bc9a09cfaeb7
PeteSalcedo <peter.salcedo.diaz@gmail.com>
2019-09-10 19:00:33 -0500

    Adding the node js

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const http = require('http');
+const bodyParser = require('body-parser')
 
-const routes = require('./routes');
+const express = require('express');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use(bodyParser.urlencoded())
+
+app.use('/add-product',(req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="sumit">Add Prduct</button></form>');
+});
+
+app.use('/product',(req, res, next) => {
+    console.log('I sent you home!');
+    console.log(req.body)
+    res.redirect('/');
+});
+
+
+app.use('/',(req, res, next) => {
+    //console.log('In Home middleware!');
+    res.send('<h1>Hello from Home!</h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
