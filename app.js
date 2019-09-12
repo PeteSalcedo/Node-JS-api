45891a61e398fbb1722f514af663eb7558d36927
PeteSalcedo <peter.salcedo.diaz@gmail.com>
2019-09-11 21:47:30 -0500

    Adding the controller

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
-const http = require('http');
-const bodyParser = require('body-parser')
+const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.use(bodyParser.urlencoded())
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use('/add-product',(req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="sumit">Add Prduct</button></form>');
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/product',(req, res, next) => {
-    console.log('I sent you home!');
-    console.log(req.body)
-    res.redirect('/');
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/',(req, res, next) => {
-    //console.log('In Home middleware!');
-    res.send('<h1>Hello from Home!</h1>');
-});
+app.use(errorController.get404);
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
